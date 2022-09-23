e582d84dcd28fd86981e41f977320828b68850e1
Lucas <prolucasloureiro@gmail.com>
2022-09-23 10:12:01 -0300

    Continuei básico em GO

---
+++ Básico/Tipos/intro.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main(){
+
+	x:= 3 //Declaração e atribuição imediata da variável.
+
+	y:= 5
+
+
+	fmt.Printf("%d + %d = %d", x, y, x+ y) //Printf funciona exatamente como em C.
+	
+}
---
+++ Básico/Tipos/types.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main(){
+
+	x:= 3
+
+	y:= 5
+
+
+	fmt.Printf("%d + %d = %d", x, y, x+ y)
+	
+}
