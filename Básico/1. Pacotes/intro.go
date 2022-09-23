513717ca167b6bfaa06b6ee576a6fd47f6271666
Lucas <prolucasloureiro@gmail.com>
2022-09-23 13:40:11 -0300

    Iniciei variáveis

--- Básico/1. Pacotes/intro.go
+++ Básico/1. Pacotes/intro.go
@@ -6,15 +6,22 @@ package main
 *Criando módulo: go mod init NOME_DO_MODULO
 *Go build: Compila tudo que está dentro do projeto com o nome do módulo.
 * Um build da main compila todos os códigos presentes aqui (inclusive os importados)
+* go mod tidy: remove as dependências que não estão sendo usadas.
 */
 
 import(
 	"fmt"
-	"modulo/auxiliar" //Importando outro arquivo go do módulo.
+	"modulo/auxiliar" //Importando outro arquivo go do módulo, pra chamar uma função deve ser usado o que está na última barra.
+	"github.com/badoux/checkmail" //Módulo externo visto na aula
 )
 
 func main(){
 
 	fmt.Println("Escrevendo da main!")
 	auxiliar.Escrever()
+
+	resultado:= checkmail.ValidateFormat("teste@gmail.com")
+
+	fmt.Println(resultado) //Retorna nulo pq não tem erro.
+
 }
