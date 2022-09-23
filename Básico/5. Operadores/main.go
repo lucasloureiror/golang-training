eac15ca6f080db5564c05df681dc213851598028
Lucas <prolucasloureiro@gmail.com>
2022-09-23 15:37:48 -0300

    Terminei operadores

---
+++ Básico/5. Operadores/main.go
@@ -0,0 +1,57 @@
+package main
+
+import "fmt"
+
+
+/*
+
+
+															Operadores Aritméticos
+1.Soma: +
+2.Subtração: -
+3.Multiplicação: *
+4.Divisão: /
+5.Resto da divisão: %
+6.Cuidados com: Tipos com bits diferentes não podem realizar operações, pois Go é fortemente tipado.
+
+															Operadores de Atribuição
+1. = Atribui o valor a variável.
+2. := Cria a variável e atribui o valor.
+
+
+
+															Operadores Relacionais
+1.Maior(>)
+2.Maior igual(>=)
+3.Igual(==)
+4.Menor(<)
+5.Menor igual(<=)
+6.Diferente(!=)
+
+
+															Operadores Lógicos
+1.E(&&)
+2.Ou(||)
+3.Negação(!): Inverte o valor booleano de algo
+
+
+
+															Operadores Unários
+1.Acréscimo(++)
+2.Acréscimo com soma(+=): num = num + x --> num += x
+2.Decréscimo(--)
+
+
+
+													Operadores Ternários NÃO EXISTEM EM GO
+
+
+
+*/
+
+func main(){
+
+	fmt.Println("Lembrando que as operações relacionam retornam true ou false")
+
+	fmt.Println(1 == 1) //Imprime true
+}
