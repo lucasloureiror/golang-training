d8093c1364bcab4ea3cec78bd16b9318beccbe1a
Lucas <prolucasloureiro@gmail.com>
2022-09-23 17:57:03 -0300

    aula: terminei array interno

--- Básico/9. Arrays/array.go
+++ Básico/9. Arrays/array.go
@@ -8,7 +8,7 @@ import "fmt"
 																Arrays em GO
 	1. Array: Declarado de forma muito parecida com C, porém com a única diferença que o tipo de dado vem depois.
 	2. Slices: Estrutura baseada na array, porém com tamanho variável. É uma estrutura de dados que pode crescer e diminuir de tamanho.
-
+		a.Arrays Internos: Quando um slice é criado, ele cria um array interno, que é o que armazena os dados.
 */
 
 
@@ -34,6 +34,14 @@ func main(){
 	array2[1] = 100 //Altera o valor do array.
 
 	fmt.Println(slice2) //Imprime o slice com o valor alterado da array 2, mostrando que na verdade existia um ponteiro para ela.
+
+	//Arrays internos
+	fmt.Println("-------------------")
+
+	slice3 := make([] float32, 10, 11) //Cria um slice com 10 posições e um array interno com 11 posições.
+	fmt.Println(slice3)
+	fmt.Println(len(slice3)) //Imprime o tamanho do slice.
+	fmt.Println(cap(slice3)) //Imprime o tamanho do array interno, mas sempre que passar a capacidade ele fornece mais.
 
 }
 
